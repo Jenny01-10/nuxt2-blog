8981bd70d5c79a41895cd72c17f4b364dcbb34f9
Jenny01-10 <84757077+Jenny01-10@users.noreply.github.com>
2022-03-26 23:31:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export default {
-  title: 'yun yu yuan',
-  githubName: 'yunyuyuan',
+  title: '小珍妮的博客',
+  githubName: 'Jenny01-10',
   githubRepo: 'nuxt2-blog',
   githubBranch: 'master',
   domain: 'https://blog.halberd.cn',
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  SEO_description: '小珍妮的变成学习之路',
+  SEO_keywords: 'Jenny\'s blog,小珍妮的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc"
 }
